d15ba74fc217252c263d4c923afe1356a42ba747
maintcode <jmwai2007@gmail.com>
2023-06-15 18:56:26 -0700

    Low level coding

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("You are not allowed to use the function puts,\n");
+    printf("But here's a piece of art with proper grammar.\n");
+    printf("This is a beautiful masterpiece!\n");
+    
+    return 0;
+}
