c4aecd3f47d447526d82eea15c7d19a0d73240fc
maintcode <jmwai2007@gmail.com>
2023-08-28 20:02:07 -0700

    Late third

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,11 @@
 
 /* Function prototype for _memset */
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+
+
+
 
 #endif /* MAIN_H */
