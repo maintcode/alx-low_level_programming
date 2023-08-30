20d2388ad883614c7248dc6248aae7c305b5a2b0
maintcode <jmwai2007@gmail.com>
2023-08-29 19:52:39 -0700

    My puts() commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+int main()
+{
+	char string[] = "programming is like building a multilingual puzzle";
+	puts(string);
+
+	return 0;
 }
