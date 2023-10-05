754d7fb2e17d5bab801e5663f0f70481da70c217
maintcode <jmwai2007@gmail.com>
2023-10-04 23:27:22 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    char digit;
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the hexadecimal numbers from 0 to 9
+ *              and the lowercase letters 'a' to 'f' followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char digit;
 
-    for (digit = '0'; digit <= '9'; digit++) {
-        putchar(digit);
-    }
+	/* Print hexadecimal numbers from 0 to 9 */
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
 
-    for (digit = 'a'; digit <= 'f'; digit++) {
-        putchar(digit);
-    }
+	/* Print lowercase letters 'a' to 'f' */
+	for (digit = 'a'; digit <= 'f'; digit++)
+	{
+		putchar(digit);
+	}
 
-    putchar('\n');
-    return 0;
+	putchar('\n');
+	return (0);
 }
