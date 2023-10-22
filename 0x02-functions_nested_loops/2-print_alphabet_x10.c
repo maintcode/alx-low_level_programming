7b4afb49a00bec0b7cf4f1027e5db161180d93f1
maintcode <jmwai2007@gmail.com>
2023-10-22 07:42:28 -0700

    Third challenge of nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ * print_alphabet_x10 -
+ * Prints the alphabet in lowercase 10 times
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
 
-    for (i = 0; i < 10; i++) // Loop 10 times
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+	}
 }
