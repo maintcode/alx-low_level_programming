768b5aa4c4694915fcd4ded800aefb37baa41088
maintcode <jmwai2007@gmail.com>
2023-10-23 21:05:57 -0700

    Check if is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is lowercase or uppercase, 0 otherwise
+ */
+int _islower(int c)
+{
+    if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        return (1);
+    return (0);
+}
