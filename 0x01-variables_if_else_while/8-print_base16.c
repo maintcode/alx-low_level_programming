999929eb235f163ce94c4d2be8259fb931c5ce20
maintcode <jmwai2007@gmail.com>
2023-10-26 12:08:30 -0700

    Patience persistance perspiration

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,13 +12,11 @@ int main(void)
 {
 	char digit;
 
-	/* Print hexadecimal numbers from 0 to 9 */
 	for (digit = '0'; digit <= '9'; digit++)
 	{
 		putchar(digit);
 	}
 
-	/* Print lowercase letters 'a' to 'f' */
 	for (digit = 'a'; digit <= 'f'; digit++)
 	{
 		putchar(digit);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    int digit;
+/**
+ * main - Entry Point of the program
+ *
+ * Description: Prints all combinations of number
+ *
+ * Return: Always 0 if success
+ *
+ */
 
-    for (digit = 0; digit < 10; digit++) {
-        putchar(digit + '0');
-        if (digit < 9) {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+int main(void)
+{
+	int digit;
 
-    putchar('\n');
-    return 0;
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+
+		if (digit < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
