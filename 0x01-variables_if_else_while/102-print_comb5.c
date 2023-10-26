11de4ff3cdc2e83fd57b5ba72b24123f3a68a98c
maintcode <jmwai2007@gmail.com>
2023-10-26 12:25:12 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    int tens1, ones1, tens2, ones2;
+/**
+ * main - Entry point of program
+ * Description: Prints all possible combination of two characters
+ * Return: Always 0 (success)
+ */
 
-    for (tens1 = 0; tens1 < 10; tens1++) {
-        for (ones1 = 0; ones1 < 10; ones1++) {
-            for (tens2 = tens1; tens2 < 10; tens2++) {
-                for (ones2 = (tens1 == tens2) ? ones1 + 1 : 0; ones2 < 10; ones2++) {
-                    putchar(tens1 + '0');
-                    putchar(ones1 + '0');
-                    putchar(' ');
-                    putchar(tens2 + '0');
-                    putchar(ones2 + '0');
-                    if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9)) {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
+int main(void)
+{
+	int tens1, ones1, tens2, ones2;
 
-    putchar('\n');
-    return 0;
+	for (tens1 = 0; tens1 < 10; tens1++)
+	{
+		for (ones1 = 0; ones1 < 10; ones1++)
+		{
+			for (tens2 = tens1; tens2 < 10; tens2++)
+			{
+				for (ones2 = (tens1 == tens2) ? ones1 + 1 : 0; ones2 < 10; ones2++)
+				{
+					putchar(tens1 + '0');
+					putchar(ones1 + '0');
+					putchar(' ');
+					putchar(tens2 + '0');
+					putchar(ones2 + '0');
+
+					if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
