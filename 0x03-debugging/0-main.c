ed1fdb0723935c81b47d0d7cd3f5370fc67d0d1c
maintcode <jmwai2007@gmail.com>
2023-10-31 10:36:17 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests a function that prints if a number is positive, neg\
+ative, or zero
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
