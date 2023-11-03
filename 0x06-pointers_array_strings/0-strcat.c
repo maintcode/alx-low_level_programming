27920840c2b276ce733b00f9e63e584e11c9be5f
maintcode <jmwai2007@gmail.com>
2023-11-03 09:21:45 -0700

    Concatinate strings

--- 0x06-pointers_array_strings/0-strcat.c
+++ 0x06-pointers_array_strings/0-strcat.c
@@ -1,29 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string where src will be added.
- * @src: The source string to be added to dest.
- * Return: A pointer to the resulting string dest.
- */
-
+  * _strcat - Concatenates two strings
+  * @dest: The destination string
+  * @src: The source string
+  *
+  * Return: A pointer to the resulting string dest
+  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = 0;
-	int src_len = 0;
+	int dlen = 0, i;
 
-	while (dest[dest_len] != '\0')
-		dest_len++;
-
-	while (src[src_len] != '\0')
-		src_len++;
-
-	for (int i = 0; i < src_len; i++)
+	while (dest[dlen])
 	{
-		dest[dest_len + i] = src[i];
+		dlen++;
 	}
 
-	dest[dest_len + src_len] = '\0';
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
 
+	dest[dlen] = '\0';
 	return (dest);
 }
