6beaf1bdee97c031cb2797446650ecac63297af6
maintcode <jmwai2007@gmail.com>
2023-11-03 08:57:52 -0700

    Concatinate two strings

--- 0x06-pointers_array_strings/0-strcat.c
+++ 0x06-pointers_array_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - Concatenates two strings.
@@ -6,6 +7,7 @@
  * @src: The source string to be appended to dest.
  * Return: A pointer to the resulting string dest.
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int dest_len = 0;
