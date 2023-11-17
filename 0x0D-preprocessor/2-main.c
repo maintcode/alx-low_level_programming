93bbc8203f7a5b34c8ea48f298dbbf5b3853c845
maintcode <jmwai2007@gmail.com>
2023-11-17 07:13:43 -0800

    Preprocessor Entry

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * @void: Empty
+ * Return: On success returns 0
+ */
+int main(void) {
+    printf("Compiled from file: %s\n", __FILE__);
+    return (0);
+}
