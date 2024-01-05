ea9283c419a41e03987ac3c8cd3f3bc2b8517f0d
maintcode <jmwai2007@gmail.com>
2024-01-05 04:53:56 -0800

    more on iteration

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,22 @@
 
 int main(void)
 {
-	int tens1, ones1, tens2, ones2;
+	int tens1, ones1;
 
-	for (tens1 = 0; tens1 < 10; tens1++)
+	for (tens1 = 0; tens1 < 99; tens1++)
 	{
-		for (ones1 = 0; ones1 < 10; ones1++)
+		for (ones1 = tens1; ones1 < 99; ones1++)
 		{
-			for (tens2 = tens1; tens2 < 10; tens2++)
-			{
-				for (ones2 = (tens1 == tens2) ? ones1 + 1 : 0; ones2 < 10; ones2++)
-				{
-					putchar(tens1 + '0');
-					putchar(ones1 + '0');
-					putchar(' ');
-					putchar(tens2 + '0');
-					putchar(ones2 + '0');
+			putchar((tens1 / 10) + '0');
+			putchar((tens1 % 10) + '0');
+			putchar(' ');
+			putchar((ones1 / 10) + '0');
+			putchar((ones1 % 10) + '0');
 
-					if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			if (tens1 != 98 || ones1 != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
