2f8acfd74b0996ce9064e4fc736c0cfe1ff78571
maintcode <jmwai2007@gmail.com>
2024-01-22 01:25:20 -0800

    Prints the name of file compiled

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: On success returns 0
+ */
+
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
