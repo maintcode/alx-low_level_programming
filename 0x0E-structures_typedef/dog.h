998bfbbce8612298f898d7d26d342f5e035b1a34
maintcode <jmwai2007@gmail.com>
2024-01-22 09:05:30 -0800

    Outside of a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,9 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
+
+/* Define dog_t as a new name for struct dog */
+typedef struct dog dog_t;
 
 #endif /* DOG_H */
