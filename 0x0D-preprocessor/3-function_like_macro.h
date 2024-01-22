8c29e16c66caa0bcba38fb25ba5a141110062305
maintcode <jmwai2007@gmail.com>
2024-01-22 01:44:06 -0800

    ABS

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,12 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef ABS_MACRO_H
+#define ABS_MACRO_H
 
-#define ABS(x) ((x) < 0 ? - ((x) * -1) : (x))
+/**
+ * ABS - Computes the absolute value of a number x.
+ * @x: The number to compute the absolute value for.
+ *
+ * Return: The absolute value of x.
+ */
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /*MYHEADER_H*/
+#endif /* ABS_MACRO_H */
