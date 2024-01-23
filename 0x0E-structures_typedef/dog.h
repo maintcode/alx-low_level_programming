d14dd6d8ab08943140110e6824e114797c458a0d
maintcode <jmwai2007@gmail.com>
2024-01-22 18:26:13 -0800

    4th try

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,18 +7,26 @@
  * @age: The age of the dog (float).
  * @owner: The owner of the dog (string).
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 /* Define dog_t as a new name for struct dog */
-typedef struct dog dog_t;
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the new dog.
+ * @age: The age of the new dog.
+ * @owner: The owner of the new dog.
+ *
+ * Return: Pointer to the new dog struct, or NULL if function fails.
+ */
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
