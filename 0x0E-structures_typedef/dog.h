e990463b1840be94d128f79915ab67cc58cfb07a
maintcode <jmwai2007@gmail.com>
2024-01-22 18:49:55 -0800

    The fifth

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,13 +12,13 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 /* Define dog_t as a new name for struct dog */
-
+typedef struct dog dog_t;
 /**
  * new_dog - Creates a new dog.
  * @name: The name of the new dog.
