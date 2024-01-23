7a7c5a5db280506ad169a99641a2b55d904c4015
maintcode <jmwai2007@gmail.com>
2024-01-23 10:52:39 -0800

    Args gift

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,7 @@
 /* Function prototype for _putchar */
 int _putchar(char c);
 void print_args(int argc, char *argv[]);
+int mul(int a, int b);
 
 
 #endif /* MAIN_H */
