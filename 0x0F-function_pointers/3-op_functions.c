6498b42de10b8584c7ef605097bcf35e142b0acc
maintcode <jmwai2007@gmail.com>
2024-01-24 19:04:55 -0800

    Calculator

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,7 +8,7 @@
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
 /**
@@ -18,21 +18,43 @@ int op_add(int a, int b)
  * Return: Sum of a and b
  */
 
-int op_sub(int a, int b) {
+int op_sub(int a, int b)
+{
 	return (a - b);
 }
 
-int op_mul(int a, int b) {
-    return a * b;
+/**
+ * op_mul - Subtract two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
 }
 
-int op_div(int a, int b) {
-    return a / b;
+/**
+ * op_div - Subtract two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
 }
 
-int op_mod(int a, int b) {
-    return a % b;
+/**
+ * op_mod - Subtract two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
-
-
-/* Similar implementations for op_sub, op_mul, op_div, and op_mod follow... */
